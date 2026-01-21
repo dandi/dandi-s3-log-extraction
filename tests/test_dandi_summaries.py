1875df0a85b19ea490467eb865a7ec5e8022e4b4
Cody Baker <cody.c.baker.phd@gmail.com>
2026-01-20 23:58:32 -0500

    debugging

--- tests/test_dandi_summaries.py
+++ tests/test_dandi_summaries.py
@@ -23,14 +23,14 @@ def test_dandiset_summaries(tmpdir: py.path.local):
 
     dandi_s3_log_extraction.summarize.generate_dandiset_summaries(cache_directory=test_dir, workers=1)
     assert test_summary_dir.is_dir()
-    assert len(test_summary_dir.glob("*.tsv")) > 0
+    assert len(list(test_summary_dir.rglob("*.tsv"))) > 0
 
     # Cannot generate `by_region.tsv` here since it would need to expose a real IP
     # TODO: disconnect IP cache and allow it to be specific in tests with false values
     # shutil.copyfile(src=expected_summaries_dir / "000126" / "by_region.tsv", dst=test_summary_dir / "000126")
 
     dandi_s3_log_extraction.summarize.generate_dandiset_totals(summary_directory=test_summary_dir)
-    assert (test_summary_dir / "totals.tsv").is_file()
+    assert (test_summary_dir / "totals.json").is_file()
 
     s3_log_extraction.summarize.generate_archive_summaries(summary_directory=test_summary_dir)
 
@@ -41,7 +41,7 @@ def test_dandiset_summaries(tmpdir: py.path.local):
     assert set(test_file_paths.keys()) == set(expected_file_paths.keys())
 
     for expected_file_path in expected_file_paths.values():
-        relative_file_path = expected_file_path.relative_to(expected_output_folder_path)
+        relative_file_path = expected_file_path.relative_to(expected_summaries_dir)
         test_file_path = test_summary_dir / relative_file_path
 
         test_mapped_log = pandas.read_table(filepath_or_buffer=test_file_path, index_col=0)
