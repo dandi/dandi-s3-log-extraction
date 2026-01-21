47d6a96ce2471e02d63777b820a39081c09f6772
Cody Baker <cody.c.baker.phd@gmail.com>
2026-01-20 22:09:20 -0500

    fix names

--- src/dandi_s3_log_extraction/_hidden_top_level_imports.py
+++ src/dandi_s3_log_extraction/_hidden_top_level_imports.py
@@ -4,4 +4,6 @@ Including these directly within the top-level `__init__.py` makes them visible t
 But we only want the imports to trigger, not for them to actually be exposed.
 """
 
+from .extractors import DandiS3LogAccessExtractor
+
 _hide = True
--- tests/test_dandi_summaries.py
+++ tests/test_dandi_summaries.py
@@ -4,9 +4,9 @@ import shutil
 import pandas
 import py
 import pytest
+import s3_log_extraction
 
 import dandi_s3_log_extraction
-import s3_log_extraction
 
 
 @pytest.mark.dandi
